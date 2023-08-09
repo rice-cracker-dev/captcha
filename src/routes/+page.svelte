49b130d8c5ea6f1c2a08cec5be4f96882e28a22f
rice-cracker-dev <ricecracker2234@outlook.com>
2023-08-09 20:39:59 +0700

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<div class="container">
+  <div>
+    <p>Please verify that you're a human</p>
+    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
+      <img src="/captcha.png" alt="" style="width: 20rem;" />
+    </a>
+  </div>
+</div>
+
+<style>
+  * {
+    box-sizing: border-box;
+  }
+
+  .container {
+    width: 100svw;
+    height: 100svh;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
